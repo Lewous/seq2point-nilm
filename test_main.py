c43c1d7e05156290648a87c2eb8810f29929d96c
Lewous <lmnsodv@gmail.com>
2020-12-28 21:04:55 +0800

    change code habit

--- test_main.py
+++ test_main.py
@@ -1,63 +1,71 @@
 import argparse
 from remove_space import remove_space
 from seq2point_test import Tester
+from matplotlib import pyplot as plt
+import pandas as pd
 
 # Allows a model to be tested from the terminal.
 
-n_app = 'freezer'
+# You need to input your test data directory
+test_directory="~/mingjun/research/housedata/refit/kettle/kettle_test_H2.csv"
 
-parser = argparse.ArgumentParser(
-    description="Train a pruned neural network for energy disaggregation. ")
+parser = argparse.ArgumentParser(description="Train a pruned neural network for energy disaggregation. ")
 
-parser.add_argument("--appliance_name", type=remove_space, default=n_app,
-                    help="The name of the appliance to perform disaggregation with. Default is kettle. Available are: kettle, fridge, dishwasher, microwave. ")
-parser.add_argument("--batch_size", type=int, default="1000",
-                    help="The batch size to use when training the network. Default is 1000. ")
-parser.add_argument("--crop", type=int, default="10000",
-                    help="The number of rows of the dataset to take training data from. Default is 10000. ")
-parser.add_argument("--algorithm", type=remove_space, default="seq2point",
-                    help="The pruning algorithm of the model to test. Default is none. ")
-parser.add_argument("--network_type", type=remove_space, default="",
-                    help="The seq2point architecture to use. Only use if you do not want to use the standard architecture. Available are: default, dropout, reduced, and reduced_dropout. ")
-parser.add_argument("--input_window_length", type=int, default="599",
-                    help="Number of input data points to network. Default is 599. ")
-parser.add_argument("--test_directory", type=str, default="",
-                    help="The dir for training data. ")
+parser.add_argument("--appliance_name", type=remove_space, default="kettle", help="The name of the appliance to perform disaggregation with. Default is kettle. Available are: kettle, fridge, dishwasher, microwave. ")
+parser.add_argument("--batch_size", type=int, default="1000", help="The batch size to use when training the network. Default is 1000. ")
+parser.add_argument("--crop", type=int, default="10000", help="The number of rows of the dataset to take training data from. Default is 10000. ")
+parser.add_argument("--algorithm", type=remove_space, default="seq2point", help="The pruning algorithm of the model to test. Default is none. ")
+parser.add_argument("--network_type", type=remove_space, default="", help="The seq2point architecture to use. Only use if you do not want to use the standard architecture. Available are: default, dropout, reduced, and reduced_dropout. ")
+parser.add_argument("--input_window_length", type=int, default="599", help="Number of input data points to network. Default is 599. ")
+parser.add_argument("--test_directory", type=str, default=test_directory, help="The dir for training data. ")
 
 arguments = parser.parse_args()
 
+# You need to provide the trained model
+saved_model_dir = "saved_models/" + arguments.appliance_name + "_" + arguments.algorithm + "_model.h5"
 
-def do1(ind):
+# The logs including results will be recorded to this log file
+log_file_dir = "saved_models/" + arguments.appliance_name + "_" + arguments.algorithm + "_" + arguments.network_type + ".log"
+
+
+def do1(name_app):
     """
     docstring
     """
+    file_directory = "./__data__/exm" + '/'.join([r'j', name_app, name_app])
 
-    global n_app
-    global arguments
+    args = {
+        'appliance': name_app, 
+        'algorithm': 'seq2point',
+        'crop': 1267000,
+        'batch_size': 130, 
+        'network_type': "",
+        'test_directory': file_directory + "_test_S(14, 15, 16).csv",
+        'saved_model_dir': file_directory + "_" + 'seq2point' + "_model.h5",
+        'log_file_dir': file_directory + "_" + 'seq2point' + "_.log", 
+        'input_window_length': 599,
+    }
+    tester = Tester(**args)
 
-    file_directory = "./__data__/exm" + '/'.join([str(ind), n_app, n_app])
-
-    # You need to input your test data directory
-    test_directory = file_directory + "_test_.csv"
-
-    # You need to provide the trained model
-    saved_model_dir = file_directory + "_" + arguments.algorithm + "_model.h5"
-
-    # The logs including results will be recorded to this log file
-    log_file_dir = "saved_models/" + arguments.appliance_name + "_" + \
-        arguments.algorithm + "_" + arguments.network_type + ".log"
-
-    tester = Tester(arguments.appliance_name, arguments.algorithm, arguments.crop,
-                    arguments.batch_size, arguments.network_type,
-                    test_directory, saved_model_dir, log_file_dir,
-                    arguments.input_window_length
-                    )
-    tester.test_model()
-
-    return 0
-
+    # tester = Tester(arguments.appliance_name, arguments.algorithm, arguments.crop, 
+    #                 arguments.batch_size, arguments.network_type,
+    #                 arguments.test_directory, saved_model_dir, log_file_dir,
+    #                 arguments.input_window_length
+    #                 )
+    get = tester.test_model()
+    return get
 
 if __name__ == "__main__":
-
-    for ind in (0, ):
-        do1(ind)
+    
+    for app in ('Fridge', ):
+        history, target = do1(app)
+        # plt.plot(target)
+        # plt.plot(history)
+        # plt.show()
+        x = pd.DataFrame(history)
+        x.to_csv('h_Fridge_h.csv', header = 0, index=0)
+        y = pd.DataFrame(target)
+        y.to_csv('h_Fridge_t.csv', header = 0, index=0)
+        print(f'{type(history)}')
+        print('\t ' + f'{x.size=}')
+        print('\t ' + str(x.size/1266760))
--- train_main.py
+++ train_main.py
@@ -3,53 +3,57 @@ from remove_space import remove_space
 from seq2point_train import Trainer
 # Allows a model to be trained from the terminal.
 
-n_app = 'freezer'
+training_directory="~/mingjun/research/housedata/refit/kettle/kettle_training_.csv"
+validation_directory="~/mingjun/research/housedata/refit/kettle/kettle_validation_.csv"
 
 parser = argparse.ArgumentParser(description="Train sequence-to-point learning for energy disaggregation. ")
 
-parser.add_argument("--appliance_name", type=remove_space, default=n_app,
-                    help="The name of the appliance to train the network with. Default is kettle. Available are: kettle, fridge, washing machine, dishwasher, and microwave. ")
-parser.add_argument("--batch_size", type=int, default="1000",
-                    help="The batch size to use when training the network. Default is 1000. ")
-parser.add_argument("--crop", type=int, default="1000",
-                    help="The number of rows of the dataset to take training data from. Default is 10000. ")
-parser.add_argument("--network_type", type=remove_space, default="seq2point",
-                    help="The seq2point architecture to use. ")
-parser.add_argument("--epochs", type=int, default="2",
-                    help="Number of epochs. Default is 10. ")
-parser.add_argument("--input_window_length", type=int, default="599",
-                    help="Number of input data points to network. Default is 599.")
-parser.add_argument("--validation_frequency", type=int, default="1",
-                    help="How often to validate model. Default is 1. ")
+parser.add_argument("--appliance_name", type=remove_space, default="kettle", help="The name of the appliance to train the network with. Default is kettle. Available are: kettle, fridge, washing machine, dishwasher, and microwave. ")
+parser.add_argument("--batch_size", type=int, default="1000", help="The batch size to use when training the network. Default is 1000. ")
+parser.add_argument("--crop", type=int, default="1000", help="The number of rows of the dataset to take training data from. Default is 10000. ")
+#parser.add_argument("--pruning_algorithm", type=remove_space, default="default", help="The pruning algorithm that the network will train with. Default is none. Available are: spp, entropic, threshold. ")
+parser.add_argument("--network_type", type=remove_space, default="seq2point", help="The seq2point architecture to use. ")
+parser.add_argument("--epochs", type=int, default="2", help="Number of epochs. Default is 10. ")
+parser.add_argument("--input_window_length", type=int, default="599", help="Number of input data points to network. Default is 599.")
+parser.add_argument("--validation_frequency", type=int, default="1", help="How often to validate model. Default is 1. ")
+parser.add_argument("--training_directory", type=str, default=training_directory, help="The dir for training data. ")
+parser.add_argument("--validation_directory", type=str, default=validation_directory, help="The dir for validation data. ")
 
 arguments = parser.parse_args()
 
+# Need to provide the trained model
+save_model_dir = "saved_models/" + arguments.appliance_name + "_" + arguments.network_type + "_model.h5"
 
-def do1(ind):
+
+def do1(name_app):
     """
     docstring
     """
 
-    global n_app
+    # global n_app
     global arguments
 
-    file_directory = "./__data__/exm" + '/'.join([str(ind), n_app, n_app])
-    training_directory = file_directory + "_training_.csv"
-    validation_directory = file_directory + "_valid_.csv"
+    # file_directory = "./__data__/exm" + '/'.join([str(ind), n_app, n_app])
+    file_directory = "./__data__/exm" + '/'.join([r'j', name_app, name_app])
 
-    # Need to provide the trained model
-    save_model_dir = file_directory + "_" + arguments.network_type + "_model.h5"
+    args = {
+        'appliance': name_app, 
+        'batch_size': 1000, 
+        'crop': 100000, 
+        'network_type': 'seq2point', 
+        'training_directory': file_directory + "_training_.csv",
+        'validation_directory': file_directory + "_valid_S(9,).csv",
+        'save_model_dir': file_directory + "_" + 'seq2point' + "_model.h5",
+        'epochs': 10, 
+        'imput_window_length': 599, 
 
-    trainer = Trainer(n_app, arguments.batch_size, arguments.crop, arguments.network_type,
-                      training_directory, validation_directory, save_model_dir,
-                    epochs = arguments.epochs, input_window_length = arguments.input_window_length,
-                    validation_frequency = arguments.validation_frequency)
-    trainer.train_model()
+    }
 
-    return 0
+    trainer = Trainer(**args)
+    get = trainer.train_model()
 
+    return get
 
 if __name__ == "__main__":
-
-    for ind in (0, ):
-        do1(ind)
+    
+    print(do1('Fridge'))
